cdd0a1e6efd69d7505886a9705148a2fec312374
CarlosAlas <48497546+CarlosAlas-CLA@users.noreply.github.com>
2019-09-26 07:49:22 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,21 +10,22 @@ namespace ReadTextFile_WriteToTextFile
     {
         static void Main(string[] args)
         {
-            String files = @"C:\Users\example\Documents\NewT.txt";//Text file path 
+             string filePath = @"C:\Desktop\New Text Document (2).txt";//Text file path 
             //Variables
-            string word = " ";
-            string e = "exit";
+            string userInput = " ";
+            string exit = "exit";
             //Read all text 
-            string excelFile = File.ReadAllText(files);
-            Console.WriteLine(excelFile);
+            string fileOutput = File.ReadAllText(filePath);
+            Console.WriteLine(fileOutput);
             //User input
             Console.WriteLine("Enter text to write to text file");
+
             //Read all text until user enters exit.
-            while (word != e)
-            {
-                word = Console.ReadLine();
-                File.AppendAllText(files, word);
-            }
+            while (userInput != exit)
+                {
+                userInput= Console.ReadLine();
+                File.AppendAllText(filePath,userInput+"\n");
+                }
         }
     }
 }
